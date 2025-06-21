acb87a6f319f713f85c2fa39a7da5ebca11cc420
Gabriel Eudes <gaeudes@student.42.fr>
2025-06-21 18:06:22 +0200

    Better norme,

--- .build/exec/make_tree/make_utils2.d
+++
@@ -1,4 +0,0 @@
-.build/exec/make_tree/make_utils2.o: src/exec/make_tree/make_utils2.c \
-  inc/arcoms.h inc/debug.h inc/utils.h inc/types.h inc/errors.h \
-  inc/env.h inc/built_in.h inc/signal_handling.h inc/make_tree.h \
-  inc/exec_btree.h
