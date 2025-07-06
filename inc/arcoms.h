cbb1e9d20661a95673b3d4ea0ba1d0afd42e21ec
Gabriel Eudes <gaeudes@student.42.fr>
2025-07-06 14:59:14 +0200

    Fixed status/rvalue issues

--- inc/arcoms.h
+++ inc/arcoms.h
@@ -6,7 +6,7 @@
 /*   By: gaeudes <gaeudes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 14:16:44 by gaeudes           #+#    #+#             */
-/*   Updated: 2025/07/05 13:05:46 by gaeudes          ###   ########.fr       */
+/*   Updated: 2025/07/06 13:56:26 by gaeudes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,8 +48,6 @@
 
 # define PIPE_READ	0
 # define PIPE_WRITE	1
-
-# define MASK_STATUS	0xff
 
 struct s_ms
 {
